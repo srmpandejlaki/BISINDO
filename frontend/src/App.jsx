36041482f6f096609c715618904cfccb4c0613c5
Muti <mutiarapandejlaki47@gmail.com>
2026-04-12 13:19:14 +0800

    tambah alias import

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,14 @@
 import { Route, Routes, Navigate } from "react-router-dom";
+import DataCollection from "./features/data-collection/pages/DataCollection";
 
 function App() {
 
   return (
     <main>
-      
+      <Routes>
+        <Route path="/" element={<Navigate to="/data-collection" />} />
+        <Route path="/data-collection" element={<DataCollection />} />
+      </Routes>
     </main>
   )
 }
---
+++ frontend/src/features/data-collection/pages/DataCollection.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function DataCollection() {
+  return (
+    <div>
+      <h1>Data Collection</h1>
+    </div>
+  )
+}
+
+export default DataCollection
